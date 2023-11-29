ed4d8a34d442783fbe2dec4892070b9307221f1e
kinjatoro <lucaslopezvazquez1@gmail.com>
2023-11-29 17:43:25 -0300

    cors

--- app.js
+++ app.js
@@ -1,64 +1,59 @@
-var express = require('express');
-var cookieParser = require('cookie-parser');
-var bluebird = require('bluebird');
-var cors = require('cors');
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const bluebird = require('bluebird');
+const cors = require('cors');
 
-var indexRouter = require('./routes/index');
-var apiRouter = require('./routes/api'); //Custom
+const indexRouter = require('./routes/index');
+const apiRouter = require('./routes/api'); // Custom
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-// Configurar las cabeceras CORS para permitir cualquier origen
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  next();
-});
+const allowedOrigins = ['http://localhost:3000', 'https://neilo.vercel.app'];
 
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      // Comprueba si el origen de la solicitud está en la lista de orígenes permitidos
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('Acceso no permitido para este origen!'));
+      }
+    },
+    credentials: true,
+  })
+);
 
-
-
-
-//Indico las rutas de los endpoint
 app.use('/api', apiRouter);
 app.use('/', indexRouter);
 
-
-//onsole.log("processENV",process.env);
 if (process.env.NODE_ENV === 'Development') {
   require('./config').config();
 }
 
-
-//Database connection --
-var mongoose = require('mongoose')
+const mongoose = require('mongoose');
 mongoose.Promise = bluebird;
-let url = `${process.env.DATABASE1}${process.env.DATABASE2}=${process.env.DATABASE3}=${process.env.DATABASE4}`
-console.log("BD",url);
-let opts = {
-  useNewUrlParser : true, 
-  connectTimeoutMS:20000, 
-  useUnifiedTopology: true
-  };
+const url = `${process.env.DATABASE1}${process.env.DATABASE2}=${process.env.DATABASE3}=${process.env.DATABASE4}`;
+const opts = {
+  useNewUrlParser: true,
+  connectTimeoutMS: 20000,
+  useUnifiedTopology: true,
+};
 
-mongoose.connect(url,opts)
+mongoose.connect(url, opts)
   .then(() => {
-    console.log(`Succesfully Connected to theMongodb Database..`)
+    console.log(`Conexión exitosa a la base de datos de MongoDB.`);
   })
   .catch((e) => {
-    console.log(`Error Connecting to the Mongodb Database...`),
-    console.log(e)
-  })
-
-
-  const port = process.env.PORT || 3000;
-  app.listen(port, '0.0.0.0', function () {
-    console.log(`Express está escuchando en el puerto ${port}`);
+    console.log(`Error al conectar a la base de datos de MongoDB...`);
+    console.log(e);
   });
-  
 
+const port = process.env.PORT || 3000;
+app.listen(port, '0.0.0.0', function () {
+  console.log(`Express está escuchando en el puerto ${port}`);
+});
 
 module.exports = app;
